16594ec3a60546d1df1baa5f91c18690a54d5426
Damir Rysaev <freepad@ya.ru>
2024-11-20 19:45:15 +0300

    init

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+// TODO: write code here
+
+// comment this to pass build
+const unusedVariable = "variable";
+
+// for demonstration purpose only
+export default function demo(value) {
+  // debugger;
+  return `Demo: ${value}`;
+}
+
+console.log("app.js included");
